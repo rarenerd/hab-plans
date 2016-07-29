5fd0eecef88807deb95dce9d4a3f26d798266493
Kristian Vlaardingerbroek <kvlaardingerbroek@schubergphilis.com>
2016-07-29 20:41:23 +0200

    Add popt plan

---
+++ popt/plan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pkg_origin=rarenerd
+pkg_name=popt
+pkg_version=1.16
+pkg_maintainer="Kristian Vlaardingerbroek <kvlaardingerbroek@schubergphilis.com>"
+pkg_license=("MIT")
+pkg_source=http://rpm5.org/files/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=e728ed296fe9f069a0e005003c3d6b2dde3d9cad453422a10d6558616d304cc8
+pkg_deps=(core/glibc)
+pkg_build_deps=(core/gcc core/make)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
